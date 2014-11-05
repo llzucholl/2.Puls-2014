fdea56eb01445dc5b9086df4e46ef5258cf808df
llzucholl <llzucholl@gmail.com>
2014-11-05 14:53:52 -0500

    Mejoras con Prigazzi

--- static/js/formulario.js
+++ static/js/formulario.js
@@ -1,39 +1,60 @@
+// //
+// //
 var $form = $('#formulario'),
 	$titulo = $('#titulo'),
-	$url = $('#url'),
-	$button = $('#mostrar-form'),
-	$list = $('#contenido'),
-	$post = $('.item').first();
+	$url = $('#link'),
+	$primerPost = $('.item').first(),
+	$lista = $('#contenido');
 
-
-
-
-// Eventos
-function mostrarFormulario() {
-	$form.slideToggle();
-	$list.slideToggle();
-
-	return false;
+if (localStorage.getItem('autosave')) {
+	$titulo.val(sessionStorage.getItem('titulo'));
+	$url.val(sessionStorage.getItem('url'));
 }
 
-function agregarPost() {
-	var url = $url.val(),
-		titulo = $titulo.val(),
-		$clone = $post.clone();
+var id = setInterval( function(){
+	sessionStorage.setItem('titulo', $titulo.val());
+	sessionStorage.setItem('url', $url.val());
+}, 1000);
 
-	$clone.find('.titulo_item a')
+function mostrarOcultarFormulario(e) {
+	//Por alguna razón, el preventDefault no funcionó :c
+	// e.preventDefault();
+	// console.log(e);
+
+	$form.slideToggle();
+	$lista.slideToggle();
+}
+
+function agregarPost(evento) {
+
+	evento.preventDefault();
+
+	var titulo = $titulo.val(),
+		url = $url.val(),
+		clone = $primerPost.clone();
+	
+	clone.find('.titulo_item a')
 		.text(titulo)
 		.attr('href', url);
 
-	$clone.hide();
+	clone.hide()
 
-	$list.prepend($clone)
-	mostrarFormulario();
-	clone.slideDown();
+	$lista.prepend(clone)
 
-	$clone.fadeIn();
-	return false;
+	$titulo.val(''); 
+	$url.val(''); 
+
+	mostrarOcultarFormulario();
+	clone.slideDown()
 }
 
-$button.click( mostrarFormulario );
-$form.on( 'submit', agregarPost );
+$('#publicar_nav a').click( mostrarOcultarFormulario );
+$('#formulario')
+	.on('submit', agregarPost)
+	.find('#link')
+	.on('focus', function(){
+		$('#link').val('http://');
+	})
+	.on('blur', function(){
+		$('#link').val('');
+	});
